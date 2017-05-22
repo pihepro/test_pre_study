6c90d598a77642b75fd55d41b48ff1764b76ea3b
pihepro <pirjo.prosi@vamk.fi>
2017-05-22 09:41:32 +0300

    Added files

---
+++ code/ThriftClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThriftClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Apache thrift client");
+        }
+    }
+}
