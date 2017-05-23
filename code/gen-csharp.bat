f25e736f4182081440be982a2756139e3850ee31
pihepro <pirjo.prosi@vamk.fi>
2017-05-23 10:53:16 +0300

    changed files

--- code/gen-csharp.bat
+++
@@ -1,3 +0,0 @@
-thrift --gen csharp -o ..\ThriftClient Service.thrift
-thrift --gen csharp -o ..\ThriftServer Service.thrift
- 
---
+++ code/service/gen-csharp.bat
@@ -0,0 +1,3 @@
+thrift.exe --gen csharp -out ..\ThriftClient Service.thrift
+thrift.exe --gen csharp -out ..\ThriftServer Service.thrift
+ 
